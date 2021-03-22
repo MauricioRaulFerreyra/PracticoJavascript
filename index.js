4f6eee80f64692837b6a2c77a2277232fedc7242
MauricioRaulFerreyra <mauriciotauro82@hotmail.com>
2021-03-22 14:10:58 -0300

    practica de lo dado en la clase de javascript

---
+++ index.js
@@ -0,0 +1,13 @@
+
+alert(`Bienvenido nuevo usuario`)
+let nombre = prompt( `Ingrese su nombre`)
+let apellido = prompt(`Ingrese su apellido`)
+alert( `Buen dia ` + nombre + apellido)
+let edad = prompt( `Ingrese su edad`)
+
+console.log( typeof edad)
+edad = parseInt(edad)
+console.log( typeof edad)  
+
+console.log(nombre + apellido)
+alert(`Muchas Gracias ` + nombre + apellido)
