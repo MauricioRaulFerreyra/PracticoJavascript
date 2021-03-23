441e3338dd61c137632d2e76f5d8d91f4b92f08d
MauricioRaulFerreyra <mauriciotauro82@hotmail.com>
2021-03-23 12:14:38 -0300

    actualización de la primer práctica de lo dado en la clase de javascript

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 
 alert(`Bienvenido nuevo usuario`)
 let nombre = prompt( `Ingrese su nombre`)
+console.log(nombre)
 let apellido = prompt(`Ingrese su apellido`)
-alert( `Buen dia ` + nombre + apellido)
+console.log(apellido)
+alert( `Buen dia ` + nombre + ` ` + apellido)
 let edad = prompt( `Ingrese su edad`)
+console.log(edad)
 
+let fecha = new Date()
+let año = fecha.getFullYear()
+console.log(año)
 console.log( typeof edad)
 edad = parseInt(edad)
-console.log( typeof edad)  
+console.log( typeof edad) 
+let fechaNac = año - edad
 
 console.log(nombre + apellido)
-alert(`Muchas Gracias ` + nombre + apellido)
+alert(`Usted ` + nombre + ` `+ apellido + ` nacio en ` + fechaNac)
+alert(`Muchas Gracias ` + nombre +` `+ apellido) 
