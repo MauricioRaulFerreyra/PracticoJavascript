c2a8c2b8dd5576b340579fb1b727b48fa460c5cc
MauricioRaulFerreyra <mauriciotauro82@hotmail.com>
2021-04-21 13:29:04 -0300

    corrección del práctico clase obejto y arrays

--- claseObjet.js
+++ claseObjet.js
@@ -1,36 +1,64 @@
 
 // se creo un arrays
 const listaEmpresa = []
+
 // se utiliza método push
 const create = (empresa) => {
     listaEmpresa.push(empresa)
+    return listaEmpresa
 }
 
 const getAll = () => {
     return listaEmpresa
 }
 
-// se creo un objeto Empresa
-function Empresa (nombre, rubro) {
-    this.nombre = nombre;
-    this.rubro  = rubro; 
-}
+const si = "si"
+let i = "si"
 
-const si = "s"
-let i = "s"
 
-while ( i == si ) {       
-    i = prompt("¿Desea ingresar al listado de empresas? s/n")  
-    if ( i == si ) {
-        const nombre = prompt("Nombre de la empresa")
-        console.log(nombre)
-        const rubro  = prompt("Rubro de la empresa")
-        console.log(rubro)
-        let empresa = new Empresa(nombre, rubro)
-        create(empresa)
-        // lista de la empresa ingresada
-        console.log(empresa)          
+// se creo una clase Empresa
+class Empresa {
+    constructor (nombre, rubro) {
+        this.nombre = nombre;
+        this.rubro  = rubro; 
+        this.venta  = false;
+    } 
+    vendido() {
+        this.venta = true;
+        this.cant  = 1;  
     }
 }
-// lista del total de las empresas ingresadas
+
+let total = 0;
+function suma() {
+    total = total + 1;
+    return total;
+} 
+
+while ( i == si ) {     
+
+    i = prompt("¿Ingresar al listado de empresas? si/no")  
+    if ( i == si ) {
+        const nombre = prompt("Nombre de la empresa")
+        console.log("El nombre de la empresa es" +" "+ nombre)
+        const rubro  = prompt("Rubro de la empresa")
+        console.log("El rubro de la empresa" + " " + nombre+" " +"es"+" " +rubro)
+        let empresa = new Empresa(nombre, rubro)     
+        create(empresa)
+        i = prompt("¿Desea un servicio web? si/no")
+            if ( i == si ) {
+            empresa.vendido();
+            console.log("Se concreto la venta de un servicio")
+            console.log(suma())
+        }
+        // lista de la empresa ingresada
+        console.log(empresa)         
+    } else { 
+        i = prompt("¿Desea seguir con el listado? si/no") 
+    } 
+}
+
+console.log("Cantidad total de servicios vendidos es" + " "+ total)
+console.log("Cantidad de empresas ingresadas son" +" "+ listaEmpresa.length)
 console.log(listaEmpresa)
+
