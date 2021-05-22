32f463b8dba4be3aed0520895b54f22f310a0185
MauricioRaulFerreyra <mauriciotauro82@hotmail.com>
2021-05-22 16:22:47 -0300

    práctico clase 13 jquery efectos y animaciones

--- app.js
+++ app.js
@@ -4,29 +4,58 @@ $(document).ready(function () {
     //  $('.display-4').html('desde clase');
     // $('#idh1').html('desde id');
 
-    $('#idh1').addClass('text-center');   
-    
-    let titulo = $('#idh1')
+    $('header').addClass('text-center');   
+     
+    $('#titulo').css('background','gray');
+    $('#titulo').css('margin-top' ,'20px');
+    $('#titulo').css('margin-bottom','20px');
 
-    $('h1').click (function () {     
-        titulo.toggleClass('color');
+    $('#container-principal').fadeOut(function(){
+        $('h1').css('color','white');
     });
 
-    $('#boton1').click(function () { 
-        $('.botones').addClass('text-center');    
+    $("#titulo").on("click",function(){
+       
+        $('#container-principal').toggle('slow');
+        $("h1").css("color", "black");
+        $('#titulo').css('background','dimgray'); 
+
+        $('#titulo').animate({height: '50px', opacity: '0.4'}, "slow");
+        
+        $('#titulo').animate({width: '100px', opacity: '0.8'}, "slow");
+        
+        $('#titulo').animate({height: '100px', opacity: '0.4'}, "slow");
+        
+        $('#titulo').animate({width: '500px', opacity: '0.8'}, "slow");
+
+        $('#titulo').animate({height: '50px', opacity: '0.4'}, "slow");
+        
+        $('#titulo').animate({width: '300px', opacity: '0.8'}, "slow");
+        
+
     });
 
-    let botonDos = $('#boton2')
-    let botonTres = $('#boton3')
-    let contenedor = $('#container-principal')
 
-    botonDos.click(function() {
-        contenedor.hide(500);
-    });
 
-    botonTres.click(function() {
-        contenedor.show(1000);
-    });
+   
+   
+
+
+   // $('#boton1').click(function () { 
+   //     $('.botones').addClass('text-center');    
+   // });
+
+   // let botonDos = $('#boton2')
+   // let botonTres = $('#boton3')
+   // let contenedor = $('#container-principal')
+
+   // botonDos.click(function() {
+   //     contenedor.hide(500);
+   //});
+
+   // botonTres.click(function() {
+   //     contenedor.show(1000);
+   // });
     
 
 
